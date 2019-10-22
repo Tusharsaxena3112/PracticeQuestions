3c6e4d6d444f30dd0d7f2fdc6c2d0ded3f433add
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-23 00:10:43 +0530

    complete

--- src/RandomGenerator/GuessNumberMain.java
+++ src/RandomGenerator/GuessNumberMain.java
@@ -6,6 +6,6 @@ public class GuessNumberMain {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         GuessNumber number = new GuessNumber(input.nextInt());
-
+        System.out.println(number);
     }
 }
