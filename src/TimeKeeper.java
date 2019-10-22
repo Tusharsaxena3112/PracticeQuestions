ad0145d60c75450142ad1b9dc43e736e2ed62fc6
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-22 22:51:48 +0530

    adding functions in TimeKeeper.java

--- src/TimeKeeper.java
+++ src/TimeKeeper.java
@@ -1,5 +1,33 @@
 public class TimeKeeper {
-    public TimeKeeper() {
+    private double hour;
+    private double minute;
+    private double second;
 
+    public TimeKeeper(double hour, double minute, double second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public double calculateSeconds(double hour, double minute, double second) {
+        return (hour * 3600 + minute * 60 + second);
+    }
+
+    public double dayPercentage() {
+        double percentage;
+        percentage = calculateSeconds(getHour(), getMinute(), getSecond()) / (24 * 3600);
+        return percentage * 100;
+    }
+
+    public double getHour() {
+        return hour;
+    }
+
+    public double getMinute() {
+        return minute;
+    }
+
+    public double getSecond() {
+        return second;
     }
 }
