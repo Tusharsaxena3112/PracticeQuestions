271d10e9d5bcc7ad4c449c3ea2661336cf4577bf
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-22 23:21:30 +0530

    adding functions to TemperatureConverter.java

--- src/tempConverter/TemperatureConverter.java
+++ src/tempConverter/TemperatureConverter.java
@@ -1,5 +1,17 @@
 package tempConverter;
 
 public class TemperatureConverter {
+    private double temp;
 
+    public TemperatureConverter(double temp) {
+        this.temp = temp;
+    }
+
+    public double converter() {
+        return temp * (9 / 5) + 32;
+    }
+
+    public String toString() {
+        return String.format("%.1f", converter());
+    }
 }
