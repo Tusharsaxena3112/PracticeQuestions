b7292b3a6b2a2eb6e005daf7dbd1446b23e70553
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-10-24 23:18:58 +0530

    adding SumNatural.java

--- src/carComparator/CompareCar.java
+++ src/carComparator/CompareCar.java
@@ -3,7 +3,7 @@ package carComparator;
  You finally selected a car named ‘X’ from a company named ‘Y’.
  The salesperson from the showroom told you about two variants of the car, one was with a CNG/Petrol fuel engine and the other was simply a Petrol fuel engine.
  The one variant with the CNG version is more fuel-efficient than the other but obviously costs more due to the added CNG configuration.
- You know the price and fuel efficiency (in kilometres per litre, kmpl for short) of both cars.
+ You know the price and fuel efficiency (in kilometres per litre) of both cars.
  You plan to keep the car for a period of 10 years.
  Assume the price ₹80 per litre of Petrol and ₹50 per kg for CNG and usage of about 15,000 kilometres per year.
  You will pay cash for the car and do not worry about financing costs.
---
+++ src/naturalSum/SumNatural.java
@@ -0,0 +1,29 @@
+package naturalSum;
+
+/*
+Write a simple program in Java to print the sum of all the natural number up to n.
+ The value of n would be given by the user.
+The program should output something like the following:
+Suppose the value of n was 5, then the output should have been,
+	1 + 2 + 3 + 4 + 5 = 15
+ */
+public class SumNatural {
+    private int number;
+
+    public SumNatural(int n) {
+        this.number = n;
+    }
+
+    public double calculateSum() {
+        double sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum += i;
+            if (i < number) {
+                System.out.print(i + "+");
+            }
+            System.out.print("=" + sum);
+        }
+
+        return 0.0;
+    }
+}
