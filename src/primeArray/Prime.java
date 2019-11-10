3e78e8135e73c3f911974529191ed7766436986b
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-10 23:54:43 +0530

    adding Prime.java

---
+++ src/primeArray/Prime.java
@@ -0,0 +1,14 @@
+package primeArray;
+
+public class Prime {
+    private int number;
+
+    public Prime(int number) {
+        this.number = number;
+    }
+
+    public boolean[] determineArray() {
+
+
+    }
+}
