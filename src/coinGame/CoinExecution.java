201422926f741cc808a306e1da13b80bdaf74b0a
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-10 11:35:20 +0530

    adding CoinExecution.java

---
+++ src/coinGame/CoinExecution.java
@@ -0,0 +1,15 @@
+package coinGame;
+
+import java.util.Scanner;
+
+public class CoinExecution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Coin coin = new Coin();
+        System.out.println("Enter no.of times :");
+        int n = input.nextInt();
+        System.out.println("Enter State:");
+        String state = input.nextLine();
+        System.out.println(coin.tossAndCount(n, state));
+    }
+}
