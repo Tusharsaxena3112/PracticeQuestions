0d3a58d5fa152834816337257d65c5582f3105fe
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-11 00:02:57 +0530

    completing Prime.java

--- src/primeArray/Prime.java
+++ src/primeArray/Prime.java
@@ -1,14 +1,28 @@
 package primeArray;
 
+import java.util.ArrayList;
+
 public class Prime {
     private int number;
-
+    ArrayList<Boolean> arrayList = new ArrayList<>();
     public Prime(int number) {
         this.number = number;
     }
 
-    public boolean[] determineArray() {
-
-
+    public Object[] determineArray() {
+        for (int i = 0; i <= number - 1; i++) {
+            int counter = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    counter++;
+                }
+            }
+            if (counter == 2) {
+                arrayList.add(Boolean.valueOf(true));
+            } else {
+                arrayList.add(Boolean.valueOf(false));
+            }
+        }
+        return arrayList.toArray();
     }
 }
