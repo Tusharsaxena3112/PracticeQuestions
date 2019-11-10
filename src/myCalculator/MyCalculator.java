a73242dfdd7dfac0f8b3c793501bb4e48c18ce1d
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-10 11:00:00 +0530

    working on MyCalculator.java

--- src/myCalculator/MyCalculator.java
+++ src/myCalculator/MyCalculator.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class MyCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int result = 1;
         System.out.println("Enter the no.");
         int number = input.nextInt();
+        System.out.println("Enter the power:");
+        int power = input.nextInt();
         try {
             if (number < 0) {
                 throw new NegativeException("Negative values are not allowed");
+            } else {
+                for (int i = 0; i <= power; i++) {
+
+                }
             }
         } catch (NegativeException e) {
             System.out.println(e.getMessage());
         }
+
     }
 }
