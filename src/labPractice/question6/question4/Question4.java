8bebe9aed45d82ace3031adb7be52f10ed9e4707
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 20:07:50 +0530

    incomplete

--- src/labPractice/question6/question4/Question4.java
+++ src/labPractice/question6/question4/Question4.java
@@ -1,18 +1,18 @@
 package labPractice.question6.question4;
 
-import java.util.TreeSet;
+import java.util.HashSet;
 
 public class Question4 {
     public static void main(String[] args) {
-//        HashSet<Name> al = new HashSet<>();
-//        for (int i = 0; i <= 9; i++) {
-//            al.add(new Name("Tushar" + i, "Saxena" + i));
-//        }
-//        System.out.println(al);
-        TreeSet<Name> ts = new TreeSet<>();
+        HashSet<Name> al = new HashSet<>();
         for (int i = 0; i <= 9; i++) {
-            ts.add(new Name("Tushar" + i, "Saxena" + i));
+            al.add(new Name("Tushar" + i, "Saxena" + i));
         }
+        System.out.println(al);
+//        TreeSet<Name> ts = new TreeSet<>();
+//        for (int i = 0; i <= 9; i++) {
+//            ts.add(new Name("Tushar" + i, "Saxena" + i));
+//        }
     }
 
 }
