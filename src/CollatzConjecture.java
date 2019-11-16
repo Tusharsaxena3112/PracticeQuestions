de448ba9113a57347f5b109370238d2ba5acc003
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 14:10:25 +0530

    adding method getCollatzConjectureString

--- src/CollatzConjecture.java
+++ src/CollatzConjecture.java
@@ -1,3 +1,29 @@
-public class CollatzConjecture {
+import java.util.ArrayList;
 
+public class CollatzConjecture {
+    private int number;
+
+    public CollatzConjecture(int number) {
+        this.number = number;
+    }
+
+    public ArrayList<Integer> getCollatzString() {
+        ArrayList<Integer> al = new ArrayList<>();
+        if (number == 1) {
+            return al;
+        } else {
+            while (number != 1) {
+                if (number % 2 == 0) {
+                    number = number / 2;
+                    al.add(number);
+                } else if (number % 2 != 0) {
+                    number = 3 * number + 1;
+                    al.add(number);
+                } else if (number == 1) {
+                    return al;
+                }
+            }
+        }
+        return al;
+    }
 }
