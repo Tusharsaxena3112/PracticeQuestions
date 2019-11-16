34f962856085f2d722b41560e6f5e08e39e9ee5c
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 19:43:00 +0530

    adding attributes to Name.java

--- src/labPractice/question6/question4/Name.java
+++ src/labPractice/question6/question4/Name.java
@@ -1,5 +1,12 @@
 package labPractice.question6.question4;
 
 public class Name {
+    private String firstName;
+    private String lastName;
+
+    public Name(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
 }
