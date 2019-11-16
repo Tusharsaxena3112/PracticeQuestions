5fde0fa234205ccf074f20c77906e5ef52fef0bb
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 14:14:21 +0530

    complete

---
+++ src/CollatzExecution.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CollatzExecution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        CollatzConjecture cl = new CollatzConjecture(number);
+        System.out.println(cl.getCollatzString().toString());
+    }
+}
