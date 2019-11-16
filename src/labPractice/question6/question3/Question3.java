fb69d20666a6b53318b81be47b970037ae00acbf
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 19:31:06 +0530

    adding Question3.java

---
+++ src/labPractice/question6/question3/Question3.java
@@ -0,0 +1,19 @@
+package labPractice.question6.question3;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        HashSet<Character> hs = new HashSet<>();
+        HashSet<Character> hs1 = new HashSet<>();
+        for (Character i : name.toCharArray()) {
+            if (!hs.add(i)) {
+                hs1.add(i);
+            }
+        }
+    }
+
+}
