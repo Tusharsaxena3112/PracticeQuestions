3f29be4fd9fc7c2403cc6fdd3788a59bd01bd7f9
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 19:11:43 +0530

    finally complete

--- src/labPractice/question6/question1/Question1.java
+++ src/labPractice/question6/question1/Question1.java
@@ -7,7 +7,7 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine().trim();
         HashSet<Character> hs = new HashSet<>();
         for (Character i : name.toCharArray()) {
             hs.add(i);
