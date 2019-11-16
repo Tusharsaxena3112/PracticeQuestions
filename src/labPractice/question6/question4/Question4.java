bcaf6b7976f580a5889fbaa42483af994e2a45b1
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-11-16 19:50:14 +0530

    adding functionality to Question4.java

--- src/labPractice/question6/question4/Question4.java
+++ src/labPractice/question6/question4/Question4.java
@@ -1,5 +1,14 @@
 package labPractice.question6.question4;
 
+import java.util.ArrayList;
+
 public class Question4 {
+    public static void main(String[] args) {
+        ArrayList<Name> al = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            al.add(new Name("Tushar" + i, "Saxena" + i));
+
+        }
+    }
 
 }
