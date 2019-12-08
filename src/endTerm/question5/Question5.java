72b6f87edf204280cd86a6501af06b72527d63ba
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-12-08 12:47:46 +0530

    incomplete Question5.java

---
+++ src/endTerm/question5/Question5.java
@@ -0,0 +1,9 @@
+package endTerm.question5;
+
+//Design an application in java that contains a class Student having properties name
+// and percentage. Create a class Test that declares three instances of Student class.
+// The values are passed through constructor at the time of its object creation.
+// The output should
+// be the name and percentage of the students who secured percentage less than 40%.
+public class Question5 {
+}
