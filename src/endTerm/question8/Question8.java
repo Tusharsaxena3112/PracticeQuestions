82d50c6b0562e0504988423aa94f82840289ea50
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-12-08 13:18:27 +0530

    adding Question8.java

---
+++ src/endTerm/question8/Question8.java
@@ -0,0 +1,7 @@
+package endTerm.question8;
+
+//Design a java program to input a String from the user and
+// prints the frequency of each vowel in the entered String.
+public class Question8 {
+
+}
